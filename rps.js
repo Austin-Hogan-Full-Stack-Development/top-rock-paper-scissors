08aa4b5b84c15231f18ce0fbc8efad3fb5018396
Austin H <austinhogandev@gmail.com>
2022-03-04 16:30:40 -0500

    Implement cpuPlay() functionality. Move test cases to seperate file.

--- rps.js
+++ rps.js
@@ -1,10 +1,32 @@
 // console.log("External JS")
 
+
+/*
+    Generate a random number 0 to 2
+    0 = rock, 1 = paper, 2 = scissors
+    Return corresponding string
+*/
 function cpuPlay() {
-    // TODO: CPU Handling
-    return;
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+    
 }
 
+
+/*
+    Accept user decision.
+    Accept CPU decision.
+    Return result. 1 for user win, 0 for tie, -1 for cpu win.
+*/
 function playRound(playerSelection, cpuSelection) {
     if (playerSelection === cpuSelection){
         return 0;
@@ -14,25 +36,5 @@ function playRound(playerSelection, cpuSelection) {
     return -1;
 }
 
-// playRound Testing
 
-console.log("Human: Rock vs. CPU: Scissors EXPECTED: 1")
-console.log(playRound("rock", "scissors"))
-console.log("Human: scissors vs. CPU: paper EXPECTED: 1")
-console.log(playRound("scissors", "paper"))
-console.log("Human: paper vs. CPU: rock EXPECTED: 1")
-console.log(playRound("paper", "rock"))
 
-console.log("Human: Scissors vs. CPU: Rock EXPECTED: -1")
-console.log(playRound("scissors", "rock"))
-console.log("Human: paper vs. CPU: scissors EXPECTED: -1")
-console.log(playRound("paper", "scissors"))
-console.log("Human: rock vs. CPU: paper EXPECTED: -1")
-console.log(playRound("rock", "paper"))
-
-console.log("Human: Rock vs. CPU: Rock EXPECTED: 0")
-console.log(playRound("rock", "rock"))
-console.log("Human: scissors vs. CPU: scissors EXPECTED: 0")
-console.log(playRound("scissors", "scissors"))
-console.log("Human: paper vs. CPU: paper EXPECTED: 0")
-console.log(playRound("paper", "paper"))
---
+++ testing.js
@@ -0,0 +1,31 @@
+// playRound Testing
+
+function playRound(playerSelection, cpuSelection) {
+    if (playerSelection === cpuSelection){
+        return 0;
+    } else if ((playerSelection === "rock" && cpuSelection === "scissors") || (playerSelection === "scissors" && cpuSelection === "paper") || (playerSelection === "paper" && cpuSelection === "rock")) {
+        return 1;
+    }
+    return -1;
+}
+
+console.log("Human: Rock vs. CPU: Scissors EXPECTED: 1")
+console.log(playRound("rock", "scissors"))
+console.log("Human: scissors vs. CPU: paper EXPECTED: 1")
+console.log(playRound("scissors", "paper"))
+console.log("Human: paper vs. CPU: rock EXPECTED: 1")
+console.log(playRound("paper", "rock"))
+
+console.log("Human: Scissors vs. CPU: Rock EXPECTED: -1")
+console.log(playRound("scissors", "rock"))
+console.log("Human: paper vs. CPU: scissors EXPECTED: -1")
+console.log(playRound("paper", "scissors"))
+console.log("Human: rock vs. CPU: paper EXPECTED: -1")
+console.log(playRound("rock", "paper"))
+
+console.log("Human: Rock vs. CPU: Rock EXPECTED: 0")
+console.log(playRound("rock", "rock"))
+console.log("Human: scissors vs. CPU: scissors EXPECTED: 0")
+console.log(playRound("scissors", "scissors"))
+console.log("Human: paper vs. CPU: paper EXPECTED: 0")
+console.log(playRound("paper", "paper"))
